afb72c7a03b51484f21a5141b91669f7e84c65e2
harry lachenmayer <harrylachenmayer@gmail.com>
2019-02-28 13:36:35 +0000

    run tests using tape

--- App.js
+++ App.js
@@ -1,32 +1,105 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import randomAccessTest from 'random-access-test'
+import test from 'tape'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+//
+// random-access-rn-file
+//
+import randomAccess from 'random-access-storage'
+import RNFS from 'react-native-fs'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+async function open(name) {
+  const exists = await RNFS.exists(name)
+  if (!exists) {
+    await RNFS.writeFile(name, '', 'utf8')
+  }
+}
 
-type Props = {};
-export default class App extends Component<Props> {
+function fileReader(name) {
+  return randomAccess({
+    open(req) {
+      open(name).then(
+        () => {
+          req.callback(null)
+        },
+        err => {
+          req.callback(err)
+        }
+      )
+    },
+    read(req) {
+      RNFS.read(name, req.length, req.offset, 'base64').then(
+        data => {
+          const buffer = Buffer.from(data, 'base64')
+          req.callback(null, buffer)
+        },
+        err => {
+          req.callback(err)
+        }
+      )
+    },
+    write(req) {
+      const data = req.data.toString('base64')
+      RNFS.write(name, data, req.offset, 'base64').then(
+        () => {
+          req.callback(null)
+        },
+        err => {
+          req.callback(err)
+        }
+      )
+    },
+  })
+}
+//
+// end random-access-rn-file
+//
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { chunks: [], err: null }
+  }
+
+  componentDidMount() {
+    test
+      .createStream()
+      .on('data', data => {
+        this.setState(({ chunks }) => ({ chunks: chunks.concat(data) }))
+      })
+      .on('error', err => {
+        this.setState({ err })
+      })
+    randomAccessTest(
+      (name, options, callback) => {
+        const randomId = ('' + Math.random()).substr(2, 6)
+        const file = `${RNFS.DocumentDirectoryPath}/${randomId}-${name}`
+        callback(fileReader(file))
+      },
+      {
+        reopen: true,
+        content: false,
+        del: false,
+        writable: false,
+        size: false,
+        truncate: false,
+      }
+    )
+  }
+
   render() {
+    const { err, chunks } = this.state
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {err != null ? <Text>error: {err.message}</Text> : null}
+        <FlatList
+          data={chunks}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(_, i) => '' + i}
+        />
       </View>
-    );
+    )
   }
 }
 
@@ -37,14 +110,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
